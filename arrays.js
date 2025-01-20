21e523f36b15d56c08295a7202fb2c6107db12f7
Kirill Alekseev <alex19892140@inbox.ru>
2025-01-20 16:13:39 +0700

    Fix function mySlice

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 
 arr = [2, 3, 1, , , 4, 6, 3, 8, 12, , , 34, 2]
 
-const mySlice = (arr, start = 0, end = null) => {
-    end === null || end > arr.length ? end = arr.length : end
+const mySlice = (arr, start = 0, end = arr.length) => {
 
     if(start < 0 && end === arr.length) {
         return arr.filter((_, index) => index >= arr.length - (start * -1))
