7b66403b453bcb2b946bf344bc7ae92d9ce4187f
Kirill Alekseev <alex19892140@inbox.ru>
2025-01-20 13:10:09 +0700

    Create function truncateString

--- strings.js
+++ strings.js
@@ -1,8 +1,27 @@
 
-const anyString = 'some string, the length, of which should not be more than 40 characters'
+const anyString = 'some String, the! length, of which should not be more than 40 characters'
+const anyString2 = 'какая-то строка длинной больше 40 символов'
 
 const firstCharacterUppercase = (str) => {
     return str.replace(/\w/, str[0].toUpperCase()) 
 }
 
-console.log(firstCharacterUppercase(anyString))
+// console.log(firstCharacterUppercase(anyString))
+
+const truncateString = (str, quantity) => {
+    if (str.length > quantity) {
+        const regexp = /[a-zа-яё0-9-]+[^\s]/ig
+        const arrWords = str.match(regexp)
+        let newStr = ''
+        for(let i = 0; newStr.length <= quantity; i++) {
+            if(`${newStr} ${arrWords[i]}`.length > quantity) {
+                return `${newStr}...`
+            }
+            newStr = newStr.length === 0 ? `${arrWords[i]}` : `${newStr} ${arrWords[i]}`
+        }
+    } else {
+        return str
+    }
+}
+
+console.log(truncateString(anyString2, 40))
